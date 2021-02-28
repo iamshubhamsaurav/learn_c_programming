26e2128223c39867ef8e06553454063773377a3a
Shubham Saurav <shubhamsinha186@gmail.com>
2021-02-28 14:42:12 +0530

    Initial Commit

---
+++ hello.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    // int num;
+    // float f;
+    // double d;
+    char c;
+
+    // printf("Please enter a integer: ");
+    // scanf("%d", &num);
+    // printf("Number is = %d", num);
+
+    // printf("\n please enter a float: ");
+    // scanf("%f", &f);
+    // printf("\nFloatimg value = %f", f);
+
+    // printf("\nPlease enter a double value: ");
+    // scanf("%lf", &d);
+    // printf("Double : %lf", d);
+
+    printf("\nPlease enter a char value: ");
+    scanf("%c", &c);
+    printf("Char = %c", c);
+
+    return 0;
+}
