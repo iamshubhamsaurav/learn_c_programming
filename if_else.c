147cc1cb90630e1fe35d31d7da2ce3d03b0a351e
Shubham Saurav <shubhamsinha186@gmail.com>
2021-03-01 20:13:36 +0530

    if else examples

--- if_else.c
+++ if_else.c
@@ -2,6 +2,17 @@
 
 int main()
 {
+    int age;
+    printf("Please enter your age: ");
+    scanf("%d", &age);
 
+    if (age <= 18) // 1
+    {
+        printf("Age is less than or equal to 18.");
+    }
+    else
+    {
+        printf("Age is greater than 18");
+    }
     return 0;
 }
