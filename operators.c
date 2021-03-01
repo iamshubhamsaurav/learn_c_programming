afd35f4ddab76cb03b4f9e1602604c633c1d3f69
Shubham Saurav <shubhamsinha186@gmail.com>
2021-03-01 19:13:54 +0530

    Relational Operators

---
+++ operators.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main()
+{
+    // int num1, num2;
+    // int sum, sub, mul, div, rem;
+
+    // float a, b, res;
+
+    // a = 7.0;
+    // b = 2.0;
+
+    // res = a % b;
+
+    // printf("Res: %f", res);
+
+    // printf("Enter 1st number: ");
+    // scanf("%d", &num1);
+
+    // printf("\nEnter 2nd number: ");
+    // scanf("%d", &num2);
+
+    // sum = num1 + num2;
+    // sub = num1 - num2;
+    // mul = num1 * num2;
+    // div = num1 / num2;
+    // rem = num1 % num2;
+
+    // printf("\nNumber 1 = %d, Number 2 = %d", num1, num2);
+    // printf("\nAddition = %d", sum);
+    // printf("\nSubtraction = %d", sub);
+    // printf("\nMultiplication = %d", mul);
+    // printf("\nDivision = %d", div);
+    // printf("\nRemainder = %d", rem);
+
+    int a = 9;
+    int b = 5;
+    printf("Before increment and decrement");
+    printf("\nA = %d, B = %d", a, b);
+    // a++
+    // a = a + 1;
+    // b--
+    // b = b - 1;
+    a++; // a = a + 1; a = 9 + 1; a = 10;
+    b--; // b = b - 1; b = 5 - 1; b = 4;
+    printf("\nAfter increment and decrement");
+    printf("\nA = %d, B = %d", a, b);
+
+    return 0;
+}
