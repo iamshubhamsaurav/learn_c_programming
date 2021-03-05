d882e51d1f651dd20faf5d6008e76dc1942a1a7e
Shubham Saurav <shubhamsinha186@gmail.com>
2021-03-05 23:55:03 +0530

    For and While loop

---
+++ for_loop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d Hello World \n", i);
+    }
+    return 0;
+}
+
+// i = 0,  i = 1, Hello world,
+// i = 2  Hello World
+// i = 3 Hello World
+// i = 4 Hello world
+// i = 5 Hello World
---
+++ while_loop.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+
+    int i = 0;
+    while (i < 5)
+    {
+        printf("%d Hello World\n", i);
+        i++;
+    }
+    /*
+    i = 0;
+    Hello World, i = 1
+    Hello world, i = 2
+    Hello World, i = 3
+    Hello World, i = 4
+    Hello World, i = 5
+
+*/
+
+    return 0;
+}
